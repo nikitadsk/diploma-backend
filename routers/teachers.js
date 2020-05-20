09f2ebb24192998f5da94fd2e415be9a28895e2e
Nikita Ershov <nikitadsk@mail.ru>
2020-05-21 00:08:29 +0300

    Added eslint Create ENDPOINTS for TEACHERS

--- routers/teachers.js
+++ routers/teachers.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
@@ -7,6 +8,27 @@ const Teacher = require('../models/Teacher');
 router.get('/teachers', auth, async (req, res) => {
     const teachers = await Teacher.find();
     res.send(teachers);
+});
+
+router.get('/teachers/:teacherId', auth, async (req, res) => {
+   const teacher = await Teacher.findById(req.params.teacherId);
+   res.send(teacher);
+});
+
+router.get('/teachers/by-discipline-id/:disciplineId', auth, async (req, res) => {
+    const teachers = await Teacher.find({
+        disciplineIds: {
+            $in: mongoose.Types.ObjectId(req.params.disciplineId)
+        }
+    });
+    res.send(teachers);
+});
+
+router.post('/teachers/', auth, async (req, res) => {
+    const teacher = new Teacher(req.body);
+    teacher._id = new mongoose.mongo.ObjectId();
+    await teacher.save();
+    res.sendStatus(200).send();
 });
 
 module.exports = router;
