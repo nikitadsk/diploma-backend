533e6d78e625108a669227279339bab3cbf4a9dc
Nikita Ershov <nikitadsk@mail.ru>
2020-05-31 20:43:09 +0300

    Добавлены группы

--- routers/groups.js
+++ routers/groups.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+
 const Group = require('../models/Group');
+const Teacher = require('../models/Teacher');
+const Specialty = require('../models/Specialty');
 
 router.get('/groups', auth, async (req, res) => {
     const groups = await Group.find();
+    for (const group of groups) {
+        group._doc.specialtyName = (await Specialty.findById(group.specialtyId)).specialtyName;
+        const { firstName, lastName, patronym } = await Teacher.findById(group.curatorId);
+        group._doc.curatorName = `${lastName} ${firstName} ${patronym}`;
+    }
     res.send(groups);
 });
 
