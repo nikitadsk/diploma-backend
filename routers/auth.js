1e0894dda06bad3181263a38cf804ad870c84a69
Nikita Ershov <nikitadsk@mail.ru>
2020-06-14 14:30:38 +0300

    РЕАЛИЗОВАНО ПРОВЕРКА ТОКЕНА

--- routers/auth.js
+++ routers/auth.js
@@ -37,4 +37,22 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/check-token', async (req, res) => {
+    const user = await User.findOne({ token: req.body.token });
+
+    if (!user) {
+        return res.status(401).send({
+            title: 'Ошибка авторизации',
+            message: 'Токен недействителен'
+        });
+    }
+
+    user.login = undefined;
+    user.password = undefined;
+
+    res.send({
+        user,
+    });
+});
+
 module.exports = router;
